993dca2e7b69848ca39c9045b45e9e3bc52b5a2f
zj970 <3060529292@qq.com>
2021-07-01 19:39:45 +0800

    java解法的完善

--- 1/twoSum.java
+++ 1/twoSum.java
@@ -5,27 +5,38 @@ import java.util.*;
 public class twoSum {
 	public static void main(String[] arg){
 		Scanner scanner = new Scanner(System.in);
-		int nums[] = new int[2];
+		//int nums[] = new int[100];
+		int targets[] = new int[2];
+		int numSize;
 		int target;
 		
-		System.out.println("请输入数组：");
+		System.out.printf("请输入数组大小：");
 		scanner.useDelimiter("\\s+");
+		
+		numSize = scanner.nextInt();
+
+		System.out.println("请输入数组：");
+
+		int nums[] = new int[numSize];
 
 		for (int i = 0; i<nums.length; i++)
 		{
 			nums[i] = scanner.nextInt();
 		}
+		System.out.println("请输入target值： ");
+		target = scanner.nextInt();
 		
-		System.out.println("数组为:"+nums[0]+"\t"+nums[1]);
+		targets = two(nums, target);
+		System.out.println("下标为:"+Arrays.toString(targets));
 		
 
 	}
 	//哈希表
-	public int[] two(int[] nums, int target) {
+	public static int[] two(int[] nums, int target) {
 		Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>( );
 		for (int i = 0; i < nums.length; ++i) {
 			if (hashtable.containsKey(target - nums[i])) {
-				return new int[] {hashtable.get(target-nums[i]), i};
+				return new int[] {hashtable.get(target-nums[i]+1), i+1};
 			}
 			hashtable.put(nums[i], i);
 		}
