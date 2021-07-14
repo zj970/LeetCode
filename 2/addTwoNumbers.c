142ded1206b9a249500f68559656155724164c8b
zj970 <3060529292@qq.com>
2021-07-14 22:45:09 +0800

    LeetCode两数 for (int i = 0; i < birds.Count +1; i++)之和

---
+++ 2/addTwoNumbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	struct ListNode
+	{
+		int val;
+		struct ListNode *next;
+	};
+
+	//struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
+	printf("这是一个测试\n");
+
+	return 0;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+
+
+}
