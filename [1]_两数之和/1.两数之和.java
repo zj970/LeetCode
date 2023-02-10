6b650a7144e9867d29891516efc94e240ae05fcd
zj970 <3060529292@qq.com>
2023-02-11 00:09:15 +0800

    测试

---
+++ [1]_两数之和/1.两数之和.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ * @lcpr version=21504
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [2,7,11,15]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,3]\n6\n
+// @lcpr case=end
+
+ */
+
+
