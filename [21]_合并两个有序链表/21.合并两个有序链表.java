d9e50a061965f7652342065a147370c1d0e9af45
zj970 <3060529292@qq.com>
2023-02-17 18:48:01 +0800

    。。。

---
+++ [21]_合并两个有序链表/21.合并两个有序链表.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=java
+ * @lcpr version=21601
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [1,2,4]\n[1,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n[0]\n
+// @lcpr case=end
+
+ */
+
+
