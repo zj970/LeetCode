92550185efde4f0b8919cfd3a95b24243fc70ce4
zj970 <3060529292@qq.com>
2023-02-22 22:00:19 +0800

    总共最多32位用位运算，有且仅当位1的时候数字++

--- [191]_位-1-的个数/191.位-1-的个数.java
+++ [191]_位-1-的个数/191.位-1-的个数.java
@@ -9,7 +9,13 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ret++;
+            }
+        }
+        return ret;
     }
 }
 // @lc code=end
@@ -20,20 +26,17 @@ public class Solution {
 // returnType=
 // @lcpr-div-debug-arg-end
 
-
 /*
-// @lcpr case=start
-// 00000000000000000000000000001011\n
-// @lcpr case=end
-
-// @lcpr case=start
-// 00000000000000000000000010000000\n
-// @lcpr case=end
-
-// @lcpr case=start
-// 11111111111111111111111111111101\n
-// @lcpr case=end
-
+ * // @lcpr case=start
+ * // 00000000000000000000000000001011\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // 00000000000000000000000010000000\n
+ * // @lcpr case=end
+ * 
+ * // @lcpr case=start
+ * // 11111111111111111111111111111101\n
+ * // @lcpr case=end
+ * 
  */
-
-
