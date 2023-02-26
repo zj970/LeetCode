9e76e0a07c00906ecc0720b8fad10f12a17a4073
zj970 <3060529292@qq.com>
2023-02-26 21:42:24 +0800

    现在想到的方法只有for循环实现

--- [118]_杨辉三角/118.杨辉三角.java
+++ [118]_杨辉三角/118.杨辉三角.java
@@ -1,14 +1,28 @@
 /*
  * @lc app=leetcode.cn id=118 lang=java
  * @lcpr version=21703
- *
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
  * [118] 杨辉三角
  */
 
 // @lc code=start
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-
+        //1.根据numRows生成多少个List<Integer>
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
     }
 }
 // @lc code=end
