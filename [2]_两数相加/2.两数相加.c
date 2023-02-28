7580c5d11253626f7cf291318c76752361b13bbc
zj970 <3060529292@qq.com>
2023-02-28 16:46:57 +0800

    补位置0，记录进位

---
+++ [2]_两数相加/2.两数相加.c
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=c
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    struct ListNode* dummy = malloc(sizeof(struct ListNode));
+    struct ListNode* cur = dummy;
+    int sum = 0;
+    while(l1 || l2 || t){
+        if(l1)
+        {
+            sum += l1->val;
+            l1=l1->next;
+        } 
+        if(l2)
+        {
+            sum += l2->val;
+            l2=l2->next;
+        } 
+        cur->next = malloc(sizeof(struct ListNode));
+        cur->next->val = t%10;
+        cur->next->next = NULL;
+        cur = cur->next;
+        t /= 10;
+    }
+    return dummy->next;
+}
+
+// @lc code=end
---
+++ [2]_两数相加/2.两数相加.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=java
+ * @lcpr version=21707
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [2,4,3]\n[5,6,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [0]\n[0]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [9,9,9,9,9,9,9]\n[9,9,9,9]\n
+// @lcpr case=end
+
+ */
+
+
