7d0f6f26dde14a63dd826b42dbce2880b50d1284
zj970 <3060529292@qq.com>
2023-03-08 20:13:15 +0800

    runtime 100,memory 78.74

---
+++ [94]_二叉树的中序遍历/94.二叉树的中序遍历.java
@@ -0,0 +1,71 @@
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ * @lcpr version=21801
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        TreeNode cur = root;
+        while(cur != null || !stack.empty()){
+            while(cur!=null){
+                stack.add(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [1,null,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
+
