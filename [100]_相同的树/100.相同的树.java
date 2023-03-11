3e20ba2bd0e6273a7b018c0122b4cc120bccb20e
zj970 <3060529292@qq.com>
2023-03-11 22:07:34 +0800

    100 ,62.54

---
+++ [100]_相同的树/100.相同的树.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=java
+ * @lcpr version=21801
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        } else if(p == null || q == null){
+            return false;
+        }
+        Queue<TreeNode> queue1 = new LinkedList<TreeNode>();
+        Queue<TreeNode> queue2 = new LinkedList<TreeNode>();
+        queue1.offer(p);
+        queue2.offer(q);
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode node1 = queue1.poll();
+            TreeNode node2 = queue2.poll();
+            if (node1.val != node2.val) {
+                return false;
+            }
+            TreeNode left1 = node1.left, right1 = node1.right, left2 = node2.left, right2 = node2.right;
+            if (left1 == null ^ left2 == null) {
+                return false;
+            }
+            if (right1 == null ^ right2 == null) {
+                return false;
+            }
+            if (left1 != null) {
+                queue1.offer(left1);
+            }
+            if (right1 != null) {
+                queue1.offer(right1);
+            }
+            if (left2 != null) {
+                queue2.offer(left2);
+            }
+            if (right2 != null) {
+                queue2.offer(right2);
+            }
+        }
+        return queue1.isEmpty() && queue2.isEmpty();
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [1,2,3]\n[1,2,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2]\n[1,null,2]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,1]\n[1,1,2]\n
+// @lcpr case=end
+
+ */
+
+
