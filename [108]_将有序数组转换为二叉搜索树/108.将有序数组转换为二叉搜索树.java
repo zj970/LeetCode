d299f3014753b66f9727d7777ad0efabecfc2dc3
zj970 <3060529292@qq.com>
2023-03-14 22:13:17 +0800

    100,87.6

---
+++ [108]_将有序数组转换为二叉搜索树/108.将有序数组转换为二叉搜索树.java
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=java
+ * @lcpr version=21801
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return Helper(nums, 0, nums.length -1);
+    }
+
+    public TreeNode Helper(int[] nums, int left, int right){
+        if (left > right) {
+            return null;
+        }
+
+        // 总是选择中间位置左边的数字作为根节点
+        int mid = (left + right) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = Helper(nums, left, mid - 1);
+        root.right = Helper(nums, mid + 1, right);
+        return root;
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// [-10,-3,0,5,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,3]\n
+// @lcpr case=end
+
+ */
+
+
