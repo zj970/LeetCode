8eccd8e477e860882433b79ed2418be505b150b6
zj970 <3060529292@qq.com>
2023-03-19 20:00:55 +0800

    47.98,70.18

---
+++ [125]_验证回文串/125.验证回文串.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=java
+ * @lcpr version=21801
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuffer sgood = new StringBuffer();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char ch = s.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                sgood.append(Character.toLowerCase(ch));
+            }
+        }
+        StringBuffer sgood_rev = new StringBuffer(sgood).reverse();
+        return sgood.toString().equals(sgood_rev.toString());
+    }
+}
+// @lc code=end
+
+// @lcpr-div-debug-arg-start
+// funName=
+// paramTypes= []
+// returnType=
+// @lcpr-div-debug-arg-end
+
+
+/*
+// @lcpr case=start
+// "A man, a plan, a canal: Panama"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "race a car"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// " "\n
+// @lcpr case=end
+
+ */
+
+
