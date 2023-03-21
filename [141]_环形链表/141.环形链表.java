6cf200e3b07452be7dd12d6bd64c9be88c13f00c
zj970 <3060529292@qq.com>
2023-03-21 13:47:38 +0800

    使用快慢指针，如果有环，证明没有next为null，有环总会让快指针等于慢指针，跳出循环

--- [141]_环形链表/141.环形链表.java
+++ [141]_环形链表/141.环形链表.java
@@ -19,14 +19,31 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> seen = new HashSet<ListNode>();
-        while (head != null) {
-            if (!seen.add(head)) {
-                return true;
-            }
-            head = head.next;
+        //  for循环
+        // Set<ListNode> seen = new HashSet<ListNode>();
+        // while (head != null) {
+        //     if (!seen.add(head)) {
+        //         return true;
+        //     }
+        //     head = head.next;
+        // }
+        // return false;
+
+        // 快慢指针
+        if (head == null || head.next == null){
+            return false;
         }
-        return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(slow != fast){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 }
 // @lc code=end
