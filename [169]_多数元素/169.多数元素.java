0fb76d6201fdc78d1afd5bb0b93412a64baee55d
zj970 <3060529292@qq.com>
2023-03-26 18:42:56 +0800

    使用投票算法时间O(n),空间O(1)

--- [169]_多数元素/169.多数元素.java
+++ [169]_多数元素/169.多数元素.java
@@ -8,8 +8,21 @@
 // @lc code=start
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+
+        //投票算法
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
     }
 }
 // @lc code=end
