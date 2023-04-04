721363a6d9be0a349986fb917fd85101d3546658
zj970 <3060529292@qq.com>
2023-04-04 14:03:01 +0800

    C语言实现

---
+++ [217]_存在重复元素/217.存在重复元素.c
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=c
+ * @lcpr version=21901
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+int cmp(const void* _a, const void* _b) {
+    int a = *(int*)_a, b = *(int*)_b;
+    return a - b;
+}
+
+bool containsDuplicate(int* nums, int numsSize){
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+    
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,1,3,3,4,3,2,4,2]\n
+// @lcpr case=end
+
+ */
+
+
