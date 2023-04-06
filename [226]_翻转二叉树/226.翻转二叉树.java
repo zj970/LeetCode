9e5ee61c89158fd7738afaf88d1d194eb863861c
zj970 <3060529292@qq.com>
2023-04-06 18:09:00 +0800

    递归遍历，叶子节点先翻转

---
+++ [226]_翻转二叉树/226.翻转二叉树.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ * @lcpr version=21901
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if  (root == null)
+        {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,2,7,1,3,6,9]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [2,1,3]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+ */
+
+
