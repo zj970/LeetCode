7f106fa4f85d7e76aa2bbf02ff51fec45706237f
zj970 <3060529292@qq.com>
2023-04-07 17:40:52 +0800

    官方的更加简洁，效率更高。思路相同，判断不同

--- [228]_汇总区间/228.汇总区间.java
+++ [228]_汇总区间/228.汇总区间.java
@@ -7,44 +7,72 @@
 // @lc code=start
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List <String> ranges = new ArrayList<String>();
-        if(nums.length == 0 || nums == null)
+        /**
+         * 个人题解 
+         */ 
+        // List <String> ranges = new ArrayList<String>();
+        // if(nums.length == 0 || nums == null)
+        // {
+        //     return ranges;
+        // }
+        // int start = nums[0];
+        // int end = start;
+        // for(int i = 1; i < nums.length; i++)
+        // {
+        //     if(nums[i]-1 == end)
+        //     {
+        //         end++;
+        //     }else{
+
+        //         if(start != end)
+        //         {
+        //             String tmp = start + "->" + end;
+        //             ranges.add(tmp);
+        //         } else{
+        //             ranges.add(String.valueOf(start));
+        //         }
+
+        //         start = nums[i];
+        //         end = start;
+        //     }
+        // }
+        // if(nums.length >=1 && nums[nums.length -1] == end)
+        //         {
+        //             if(start != end)
+        //             {
+        //                 String tmp = start + "->" + end;
+        //                 ranges.add(tmp);
+        //             } else{
+        //                 ranges.add(String.valueOf(start));
+        //             }
+        //         }
+
+        // return ranges;
+
+        //官方实现
+
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        int n = nums.length;
+        while(i<n)
         {
-            return ranges;
-        }
-        int start = nums[0];
-        int end = start;
-        for(int i = 1; i < nums.length; i++)
-        {
-            if(nums[i]-1 == end)
+            int low = i;
+            i++;
+            while(i < n && nums[i] == nums[i-1]+1)
             {
-                end++;
-            }else{
-
-                if(start != end)
-                {
-                    String tmp = start + "->" + end;
-                    ranges.add(tmp);
-                } else{
-                    ranges.add(String.valueOf(start));
-                }
-
-                start = nums[i];
-                end = start;
+                i++;
             }
-        }
-        if(nums.length >=1 && nums[nums.length -1] == end)
-                {
-                    if(start != end)
-                    {
-                        String tmp = start + "->" + end;
-                        ranges.add(tmp);
-                    } else{
-                        ranges.add(String.valueOf(start));
-                    }
-                }
+            int high = i - 1;
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
+            if(low < high)
+            {
+                temp.append("->");
+                temp.append(Integer.toString(nums[high]));
+            }
 
-        return ranges;
+            ret.add(temp.toString());
+        }
+        return ret;
     }
 }
 // @lc code=end
