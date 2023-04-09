5fd94928c80cb823f84f689df8a4ccf7586ad4a8
zj970 <3060529292@qq.com>
2023-04-09 21:12:01 +0800

    转换成char数组排序进行比较

---
+++ [242]_有效的字母异位词/242.有效的字母异位词.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=242 lang=java
+ *
+ * [242] 有效的字母异位词
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars,tChars);
+    }
+}
+// @lc code=end
+
