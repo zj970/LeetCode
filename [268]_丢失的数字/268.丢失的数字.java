a61d474381fd17f372563fabe919d051c8df821d
zj970 <3060529292@qq.com>
2023-04-11 22:29:11 +0800

    排序，循环判断

---
+++ [268]_丢失的数字/268.丢失的数字.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=268 lang=java
+ *
+ * [268] 丢失的数字
+ */
+
+// @lc code=start
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+        return n;
+    }
+}
+// @lc code=end
+
