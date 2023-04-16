74de2ea97572f538a1b4a51eb8330ee053c714c5
zj970 <3060529292@qq.com>
2023-04-16 19:46:06 +0800

    前缀和

---
+++ [303]_区域和检索-数组不可变/303.区域和检索-数组不可变.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=303 lang=java
+ * @lcpr version=21906
+ *
+ * [303] 区域和检索 - 数组不可变
+ */
+
+// @lc code=start
+class NumArray {
+    int[] sums;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        sums = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return sums[right+1]-sums[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// ["NumArray", "sumRange", "sumRange", "sumRange"][[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]\n
+// @lcpr case=end
+
+ */
+
