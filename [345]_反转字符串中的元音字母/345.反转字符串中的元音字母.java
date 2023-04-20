83a8ddd8254f7f60c714b28f9d993ae1524b1b83
zj970 <3060529292@qq.com>
2023-04-20 22:08:43 +0800

    双指针同344

---
+++ [345]_反转字符串中的元音字母/345.反转字符串中的元音字母.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode.cn id=345 lang=java
+ * @lcpr version=21906
+ *
+ * [345] 反转字符串中的元音字母
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseVowels(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+
+        int i = 0, j = n - 1;
+        while (i < j) {
+            while (i < n && !isVowel(arr[i])) {
+                ++i;
+            }
+            while (j > 0 && !isVowel(arr[j])) {
+                --j;
+            }
+            if (i < j) {
+                swap(arr, i, j);
+                ++i;
+                --j;
+            }
+        }
+        return new String(arr);
+    }
+
+    public boolean isVowel(char ch) {
+        return "aeiouAEIOU".indexOf(ch) >= 0;
+    }
+
+    public void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// "hello"\n
+// @lcpr case=end
+
+// @lcpr case=start
+// "leetcode"\n
+// @lcpr case=end
+
+ */
+
