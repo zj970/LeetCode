c7652cb349988b137500be9f40fb7f7912f6786d
zj970 <3060529292@qq.com>
2023-04-26 18:12:53 +0800

    暴力枚举

---
+++ [401]_二进制手表/401.二进制手表.java
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=401 lang=java
+ * @lcpr version=21907
+ *
+ * [401] 二进制手表
+ */
+
+import java.util.*;
+
+/**
+ * 
+ * 二进制手表顶部有 4 个 LED 代表 小时（0-11），底部的 6 个 LED 代表 分钟（0-59）。每个 LED 代表一个 0 或 1，最低位在右侧。
+
+例如，下面的二进制手表读取 "3:25" 。
+给你一个整数 turnedOn ，表示当前亮着的 LED 的数量，返回二进制手表可以表示的所有可能时间。你可以 按任意顺序 返回答案。
+
+小时不会以零开头：
+
+例如，"01:00" 是无效的时间，正确的写法应该是 "1:00" 。
+分钟必须由两位数组成，可能会以零开头：
+
+例如，"10:2" 是无效的时间，正确的写法应该是 "10:02" 。
+ */
+// @lc code=start
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> ans = new ArrayList<String>();
+        for (int h = 0; h < 12; ++h) {
+            for (int m = 0; m < 60; ++m) {
+                if (Integer.bitCount(h) + Integer.bitCount(m) == turnedOn) {
+                    ans.add(h + ":" + (m < 10 ? "0" : "") + m);
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 9\n
+// @lcpr case=end
+
+ */
+
