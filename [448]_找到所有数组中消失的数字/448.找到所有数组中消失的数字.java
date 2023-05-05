d7946ee466a193983531abd6139e1725d0f4ad28
zj970 <3060529292@qq.com>
2023-05-05 23:20:37 +0800

    哈希表记录数组 {nums} 中的数字用哈希表检查 [1,n][1,n] 中的每一个数是否出现

---
+++ [448]_找到所有数组中消失的数字/448.找到所有数组中消失的数字.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=448 lang=java
+ * @lcpr version=21907
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+/**
+ * 给你一个含 n 个整数的数组 nums ，其中 nums[i] 在区间 [1, n] 内。请你找出所有在 [1, n] 范围内但没有出现在 nums 中的数字，并以数组的形式返回结果。
+ */
+// @lc code=start
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length;
+        for (int num : nums) {
+            int x = (num - 1) % n;
+            nums[x] += n;
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) {
+                ret.add(i + 1);
+            }
+        }
+        return ret;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [4,3,2,7,8,2,3,1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1]\n
+// @lcpr case=end
+
+ */
+
