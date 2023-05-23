9a25d201d414602fb0628c4ebe7a141ff3350acf
zj970 <3060529292@qq.com>
2023-05-23 21:11:16 +0800

    内置函数，无特殊...可以看源码实现

---
+++ [504]_七进制数/504.七进制数.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=java
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+class Solution {
+    public String convertToBase7(int num) {
+        return Integer.toString(num, 7);
+    }
+}
+// @lc code=end
+
