6088334ea42b245e9cbc70506d3405ec6b28e7d1
zj970 <3060529292@qq.com>
2023-06-24 18:31:14 +0300

    滑动窗口

---
+++ [643]_子数组最大平均数-i/643.子数组最大平均数-i.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=643 lang=java
+ *
+ * [643] 子数组最大平均数 I
+ */
+
+// @lc code=start
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int sum = 0;
+        int n = nums.length;
+        for(int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+        int maxSum = sum;
+        for (int i = k; i < n; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, sum);
+        }
+        return 1.0 * maxSum / k;
+    }
+}
+// @lc code=end
+
