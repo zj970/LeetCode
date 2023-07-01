ab337dae2a7efa263ab26c7473e4db891c058e46
zj970 <3060529292@qq.com>
2023-07-01 20:12:42 +0800

    贪心

---
+++ [674]_最长连续递增序列/674.最长连续递增序列.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=674 lang=java
+ *
+ * [674] 最长连续递增序列
+ */
+
+// @lc code=start
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        int start = 0;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] <= nums[i - 1]) {
+                start = i;
+            }
+            ans = Math.max(ans, i - start + 1);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
