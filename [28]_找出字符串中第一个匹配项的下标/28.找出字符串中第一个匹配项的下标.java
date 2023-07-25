de140ee07e57d3722e8e646e10a8c00038332e61
zj970 <3060529292@qq.com>
2023-07-25 20:57:56 +0800

    KMP经典算法

---
+++ [28]_找出字符串中第一个匹配项的下标/28.找出字符串中第一个匹配项的下标.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=28 lang=java
+ *
+ * [28] 找出字符串中第一个匹配项的下标
+ */
+
+// @lc code=start
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+// @lc code=end
+
