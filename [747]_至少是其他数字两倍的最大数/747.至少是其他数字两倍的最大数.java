efb67ae998cb203b919ef4a71ab12fc509e10f85
zj970 <3060529292@qq.com>
2023-07-29 22:52:58 +0800

    循环遍历

---
+++ [747]_至少是其他数字两倍的最大数/747.至少是其他数字两倍的最大数.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=747 lang=java
+ *
+ * [747] 至少是其他数字两倍的最大数
+ */
+
+// @lc code=start
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int m1 = -1, m2 = -1;
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > m1) {
+                m2 = m1;
+                m1 = nums[i];
+                index = i;
+            } else if (nums[i] > m2) {
+                m2 = nums[i];
+            }
+        }
+        return m1 >= m2 * 2 ? index : -1;
+    }
+}
+// @lc code=end
+
