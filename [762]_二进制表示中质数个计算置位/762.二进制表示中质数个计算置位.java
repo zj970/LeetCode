c750544ea60f1d05a60963012a412207499ec151
zj970 <3060529292@qq.com>
2023-07-31 23:29:43 +0800

    数学+位运算

---
+++ [762]_二进制表示中质数个计算置位/762.二进制表示中质数个计算置位.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=762 lang=java
+ *
+ * [762] 二进制表示中质数个计算置位
+ */
+
+// @lc code=start
+class Solution {
+    public int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for (int x = left; x <= right; ++x) {
+            if (isPrime(Integer.bitCount(x))) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private boolean isPrime(int x) {
+        if (x < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= x; ++i) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
