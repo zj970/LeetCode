01b8a6fded7456e676f08c55d125f35cb790d01d
zj970 <3060529292@qq.com>
2023-08-01 22:04:49 +0800

    循环遍历

---
+++ [766]_托普利茨矩阵/766.托普利茨矩阵.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=766 lang=java
+ *
+ * [766] 托普利茨矩阵
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
