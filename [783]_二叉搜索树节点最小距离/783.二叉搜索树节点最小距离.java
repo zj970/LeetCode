d3eb07fad0dd702e048913fd94f8a4f612b05d0d
zj970 <3060529292@qq.com>
2023-08-03 21:45:08 +0800

    中序遍历

---
+++ [783]_二叉搜索树节点最小距离/783.二叉搜索树节点最小距离.java
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=783 lang=java
+ *
+ * [783] 二叉搜索树节点最小距离
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int pre;
+    int ans;
+
+    public int minDiffInBST(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        pre = -1;
+        dfs(root);
+        return ans;
+    }
+
+    public void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        if (pre == -1) {
+            pre = root.val;
+        } else {
+            ans = Math.min(ans, root.val - pre);
+            pre = root.val;
+        }
+        dfs(root.right);
+    }
+}
+
+// @lc code=end
+
