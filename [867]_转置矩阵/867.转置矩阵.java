40472f03dcd7225b07c7b014e2d5422e91294c82
zj970 <3060529292@qq.com>
2023-08-19 20:25:50 +0800

    模拟

---
+++ [867]_转置矩阵/867.转置矩阵.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=867 lang=java
+ *
+ * [867] 转置矩阵
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[][] transposed = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+}
+
+// @lc code=end
+
