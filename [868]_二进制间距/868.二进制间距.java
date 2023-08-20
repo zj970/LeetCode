c9a677ae025085f32b8ecb65fe66aecb0c1de64d
zj970 <3060529292@qq.com>
2023-08-20 21:45:21 +0800

    位运算

---
+++ [868]_二进制间距/868.二进制间距.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=868 lang=java
+ *
+ * [868] 二进制间距
+ */
+
+// @lc code=start
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for (int i = 0; n != 0; ++i) {
+            if ((n & 1) == 1) {
+                if (last != -1) {
+                    ans = Math.max(ans, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
