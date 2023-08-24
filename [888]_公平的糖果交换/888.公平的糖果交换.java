94771f85e71b46b77c6836f4ce9bd05a28b7c1a6
zj970 <3060529292@qq.com>
2023-08-24 23:09:58 +0800

    Hash表

---
+++ [888]_公平的糖果交换/888.公平的糖果交换.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=888 lang=java
+ *
+ * [888] 公平的糖果交换
+ */
+
+// @lc code=start
+class Solution {
+    public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
+        int sumA = Arrays.stream(aliceSizes).sum();
+        int sumB = Arrays.stream(bobSizes).sum();
+        int delta = (sumA - sumB) / 2;
+        Set<Integer> rec = new HashSet<Integer>();
+        for (int num : aliceSizes) {
+            rec.add(num);
+        }
+        int[] ans = new int[2];
+        for (int y : bobSizes) {
+            int x = y + delta;
+            if (rec.contains(x)) {
+                ans[0] = x;
+                ans[1] = y;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
