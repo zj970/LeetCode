7a7501a037f03291d4f030054e5e51929ca14cbf
zj970 <3060529292@qq.com>
2023-08-25 22:45:04 +0800

    分步累加

---
+++ [892]_三维形体的表面积/892.三维形体的表面积.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=892 lang=java
+ *
+ * [892] 三维形体的表面积
+ */
+
+// @lc code=start
+class Solution {
+    public int surfaceArea(int[][] grid) {
+        int[] dr = new int[]{0, 1, 0, -1};
+        int[] dc = new int[]{1, 0, -1, 0};
+
+        int N = grid.length;
+        int ans = 0;
+
+        for (int r = 0; r < N; ++r) {
+            for (int c = 0; c < N; ++c) {
+                if (grid[r][c] > 0) {
+                    ans += 2;
+                    for (int k = 0; k < 4; ++k) {
+                        int nr = r + dr[k];
+                        int nc = c + dc[k];
+                        int nv = 0;
+                        if (0 <= nr && nr < N && 0 <= nc && nc < N) {
+                            nv = grid[nr][nc];
+                        }
+
+                        ans += Math.max(grid[r][c] - nv, 0);
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// @lc code=end
+
