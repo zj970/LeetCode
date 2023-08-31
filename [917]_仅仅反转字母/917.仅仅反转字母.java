5bb0231474c0093e42f74c992dd20923d020f718
zj970 <3060529292@qq.com>
2023-08-31 22:35:07 +0800

    双指针+扫描

---
+++ [917]_仅仅反转字母/917.仅仅反转字母.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=917 lang=java
+ *
+ * [917] 仅仅反转字母
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int left = 0, right = n - 1;
+        while (true) {
+            while (left < right && !Character.isLetter(s.charAt(left))) { // 判断左边是否扫描到字母
+                left++;
+            }
+            while (right > left && !Character.isLetter(s.charAt(right))) { // 判断右边是否扫描到字母
+                right--;
+            }
+            if (left >= right) {
+                break;
+            }
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+
+    public void swap(char[] arr, int left, int right) {
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
+// @lc code=end
+
