2eded2c3d1967fa4bd69609045096b39f690cdc3
zj970 <3060529292@qq.com>
2023-09-19 20:50:39 +0800

    贪心，排序

---
+++ [976]_三角形的最大周长/976.三角形的最大周长.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=976 lang=java
+ *
+ * [976] 三角形的最大周长
+ */
+
+// @lc code=start
+class Solution {
+    public int largestPerimeter(int[] A) {
+        Arrays.sort(A);
+        for (int i = A.length - 1; i >= 2; --i) {
+            if (A[i - 2] + A[i - 1] > A[i]) {
+                return A[i - 2] + A[i - 1] + A[i];
+            }
+        }
+        return 0;
+    }
+}
+// @lc code=end
+
