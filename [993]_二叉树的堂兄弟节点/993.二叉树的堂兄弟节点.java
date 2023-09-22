90dd6c5f9c0a5b8cb533d22d1d2758860f02f389
zj970 <3060529292@qq.com>
2023-09-22 21:01:12 +0800

    深度优先搜索

---
+++ [993]_二叉树的堂兄弟节点/993.二叉树的堂兄弟节点.java
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=993 lang=java
+ *
+ * [993] 二叉树的堂兄弟节点
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // x 的信息
+    int x;
+    TreeNode xParent;
+    int xDepth;
+    boolean xFound = false;
+
+    // y 的信息
+    int y;
+    TreeNode yParent;
+    int yDepth;
+    boolean yFound = false;
+
+    public boolean isCousins(TreeNode root, int x, int y) {
+        this.x = x;
+        this.y = y;
+        dfs(root, 0, null);
+        return xDepth == yDepth && xParent != yParent;
+    }
+
+    public void dfs(TreeNode node, int depth, TreeNode parent) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.val == x) {
+            xParent = parent;
+            xDepth = depth;
+            xFound = true;
+        } else if (node.val == y) {
+            yParent = parent;
+            yDepth = depth;
+            yFound = true;
+        }
+
+        // 如果两个节点都找到了，就可以提前退出遍历
+        // 即使不提前退出，对最坏情况下的时间复杂度也不会有影响
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.left, depth + 1, node);
+
+        if (xFound && yFound) {
+            return;
+        }
+
+        dfs(node.right, depth + 1, node);
+    }
+}
+
+// @lc code=end
+
