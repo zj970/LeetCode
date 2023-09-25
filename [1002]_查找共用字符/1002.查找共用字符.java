046e4f85a8b5bebedee9a7073421d49cb8994c16
zj970 <3060529292@qq.com>
2023-09-25 23:30:44 +0800

    计数

---
+++ [1002]_查找共用字符/1002.查找共用字符.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1002 lang=java
+ *
+ * [1002] 查找共用字符
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] minfreq = new int[26];
+        Arrays.fill(minfreq, Integer.MAX_VALUE);
+        for (String word : words) {
+            int[] freq = new int[26];
+            int length = word.length();
+            for (int i = 0; i < length; ++i) {
+                char ch = word.charAt(i);
+                ++freq[ch - 'a'];
+            }
+            for (int i = 0; i < 26; ++i) {
+                minfreq[i] = Math.min(minfreq[i], freq[i]);
+            }
+        }
+
+        List<String> ans = new ArrayList<String>();
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < minfreq[i]; ++j) {
+                ans.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
