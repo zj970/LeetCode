4998e85aa11788bc98760c2b270eeef184785904
zj970 <3060529292@qq.com>
2023-09-29 20:37:50 +0800

    遍历寻找切分点

---
+++ [1013]_将数组分成和相等的三个部分/1013.将数组分成和相等的三个部分.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=1013 lang=java
+ *
+ * [1013] 将数组分成和相等的三个部分
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canThreePartsEqualSum(int[] A) {
+        int s = 0;
+        for (int num : A) {
+            s += num;
+        }
+        if (s % 3 != 0) {
+            return false;
+        }
+        int target = s / 3;
+        int n = A.length, i = 0, cur = 0;
+        while (i < n) {
+            cur += A[i];
+            if (cur == target) {
+                break;
+            }
+            ++i;
+        }
+        if (cur != target) {
+            return false;
+        }
+        int j = i + 1;
+        while (j + 1 < n) {  // 需要满足最后一个数组非空
+            cur += A[j];
+            if (cur == target * 2) {
+                return true;
+            }
+            ++j;
+        }
+        return false;
+    }
+}
+
