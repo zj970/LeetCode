965eb146fa7f449b0697de5b82a63495c14fd71e
zj970 <3060529292@qq.com>
2023-09-30 18:54:15 +0800

    遍历

---
+++ [1018]_可被-5-整除的二进制前缀/1018.可被-5-整除的二进制前缀.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=1018 lang=java
+ *
+ * [1018] 可被 5 整除的二进制前缀
+ */
+
+// @lc code=start
+class Solution {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> answer = new ArrayList<Boolean>();
+        int prefix = 0;
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            prefix = ((prefix << 1) + nums[i]) % 5;
+            answer.add(prefix == 0);
+        }
+        return answer;
+    }
+}
+
+// @lc code=end
+
