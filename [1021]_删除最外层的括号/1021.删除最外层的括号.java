0f9fd2586f6bea8d931df9f918795b8e390c54ad
zj970 <3060529292@qq.com>
2023-10-01 20:52:43 +0800

    利用栈操作

---
+++ [1021]_删除最外层的括号/1021.删除最外层的括号.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1021 lang=java
+ *
+ * [1021] 删除最外层的括号
+ */
+
+// @lc code=start
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuffer res = new StringBuffer();
+        Deque<Character> stack = new ArrayDeque<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ')') {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                res.append(c);
+            }
+            if (c == '(') {
+                stack.push(c);
+            }
+        }
+        return res.toString();
+    }
+}
+
+// @lc code=end
+
