bea15ae424c38e88d8f4984c49eb69695d4f319b
zj970 <3060529292@qq.com>
2023-10-02 20:28:35 +0800

    递归

---
+++ [1022]_从根到叶的二进制数之和/1022.从根到叶的二进制数之和.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=1022 lang=java
+ *
+ * [1022] 从根到叶的二进制数之和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int val) {
+        if (root == null) {
+            return 0;
+        }
+        val = (val << 1) | root.val;
+        if (root.left == null && root.right == null) {
+            return val;
+        }
+        return dfs(root.left, val) + dfs(root.right, val);
+    }
+}
+
+// @lc code=end
+
