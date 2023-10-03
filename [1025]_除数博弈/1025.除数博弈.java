3dab1ed15910a095c971814e1aaaec4fd9c97cbc
zj970 <3060529292@qq.com>
2023-10-03 22:19:15 +0800

    n 为奇数的时候 Alice（先手）必败，n 为偶数的时候 Alice\text{Alice}Alice 必胜

---
+++ [1025]_除数博弈/1025.除数博弈.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=1025 lang=java
+ *
+ * [1025] 除数博弈
+ */
+
+// @lc code=start
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
+// @lc code=end
+
