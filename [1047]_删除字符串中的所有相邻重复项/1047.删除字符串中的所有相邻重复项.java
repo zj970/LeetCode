a80e55b63058a09510f190e1dad6c9c09b9fc075
zj970 <3060529292@qq.com>
2023-10-06 23:34:51 +0800

    栈

---
+++ [1047]_删除字符串中的所有相邻重复项/1047.删除字符串中的所有相邻重复项.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1047 lang=java
+ *
+ * [1047] 删除字符串中的所有相邻重复项
+ */
+
+// @lc code=start
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuffer stack = new StringBuffer();
+        int top = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (top >= 0 && stack.charAt(top) == ch) {
+                stack.deleteCharAt(top);
+                --top;
+            } else {
+                stack.append(ch);
+                ++top;
+            }
+        }
+        return stack.toString();
+    }
+}
+
+// @lc code=end
+
