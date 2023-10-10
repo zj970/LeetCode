8795fbb03da68ec7347d914fa852ea040bdc1aa2
zj970 <3060529292@qq.com>
2023-10-10 21:20:26 +0800

    双指针

---
+++ [1089]_复写零/1089.复写零.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=1089 lang=java
+ *
+ * [1089] 复写零
+ */
+
+// @lc code=start
+class Solution {
+    public void duplicateZeros(int[] arr) {
+        int n = arr.length;
+        int top = 0;
+        int i = -1;
+        while (top < n) {
+            i++;
+            if (arr[i] != 0) {
+                top++;
+            } else {
+                top += 2;
+            }
+        }
+        int j = n - 1;
+        if (top == n + 1) {
+            arr[j] = 0;
+            j--;
+            i--;
+        } 
+        while (j >= 0) {
+            arr[j] = arr[i];
+            j--;
+            if (arr[i] == 0) {
+                arr[j] = arr[i];
+                j--;
+            } 
+            i--;
+        }
+    }
+}
+// @lc code=end
+
