3aeb568a0a7d10fea193eea42a73adeb69640283
zj970 <3060529292@qq.com>
2023-10-13 22:26:13 +0800

    计数排序

---
+++ [1122]_数组的相对排序/1122.数组的相对排序.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=1122 lang=java
+ *
+ * [1122] 数组的相对排序
+ */
+
+// @lc code=start
+class Solution {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int upper = 0;
+        for (int x : arr1) {
+            upper = Math.max(upper, x);
+        }
+        int[] frequency = new int[upper + 1];
+        for (int x : arr1) {
+            ++frequency[x];
+        }
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        for (int x : arr2) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+            frequency[x] = 0;
+        }
+        for (int x = 0; x <= upper; ++x) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+        }
+        return ans;
+    }
+}
+
+// @lc code=end
+
