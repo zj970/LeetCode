cd280527137477ab7075aa3fa39f4943030d1da5
zj970 <3060529292@qq.com>
2023-10-14 22:26:16 +0800

    计数+二元组

---
+++ [1128]_等价多米诺骨牌对的数量/1128.等价多米诺骨牌对的数量.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1128 lang=java
+ *
+ * [1128] 等价多米诺骨牌对的数量
+ */
+
+// @lc code=start
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int[] num = new int[100];
+        int ret = 0;
+        for (int[] domino : dominoes) {
+            int val = domino[0] < domino[1] ? domino[0] * 10 + domino[1] : domino[1] * 10 + domino[0];
+            ret += num[val];
+            num[val]++;
+        }
+        return ret;
+    }
+}
+// @lc code=end
+
