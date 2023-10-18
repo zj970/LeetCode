c9d62f4504b9f2a81ab0accff4e35114d8c6f489
zj970 <3060529292@qq.com>
2023-10-18 23:23:31 +0800

    二分查找+位运算

---
+++ [1175]_质数排列/1175.质数排列.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=1175 lang=java
+ *
+ * [1175] 质数排列
+ */
+
+// @lc code=start
+class Solution {
+    static final int MOD = 1000000007;
+
+    public int numPrimeArrangements(int n) {
+        int numPrimes = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrime(i)) {
+                numPrimes++;
+            }
+        }
+        return (int) (factorial(numPrimes) * factorial(n - numPrimes) % MOD);
+    }
+
+    public boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public long factorial(int n) {
+        long res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+            res %= MOD;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
---
+++ [222]_Count Complete Tree Nodes/s222.Count Complete Tree Nodes.java
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=222 lang=java
+ * @lcpr version=30103
+ *
+ * [222] 完全二叉树的节点个数
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int level = 0;
+        TreeNode node = root;
+        while (node.left != null) {
+            level++;
+            node = node.left;
+        }
+        int low = 1 << level, high = (1 << (level + 1)) - 1;
+        while (low < high) {
+            int mid = (high - low + 1) / 2 + low;
+            if (exists(root, level, mid)) {
+                low = mid;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public boolean exists(TreeNode root, int level, int k) {
+        int bits = 1 << (level - 1);
+        TreeNode node = root;
+        while (node != null && bits > 0) {
+            if ((bits & k) == 0) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+            bits >>= 1;
+        }
+        return node != null;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5,6]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// []\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n
+// @lcpr case=end
+
+ */
+
