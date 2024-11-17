6af71261e19f51d4fa8986738bd479959159ab25
zj970 <3060529292@qq.com>
2024-11-17 21:58:06 +0800

    ?????

---
+++ [2235]_两整数相加/s2235.两整数相加.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=2235 lang=java
+ * @lcpr version=30204
+ *
+ * [2235] 两整数相加
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 12\n5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// -10\n4\n
+// @lcpr case=end
+
+ */
+
