8ef687e05b4f4419b461caea37d9d4fce13cfd4d
zj970 <3060529292@qq.com>
2025-01-03 23:09:52 +0800

    这题来搞笑的吧

---
+++ [2549]_统计桌面上的不同数字/s2549.统计桌面上的不同数字.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=2549 lang=java
+ * @lcpr version=30204
+ *
+ * [2549] 统计桌面上的不同数字
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int distinctIntegers(int n) {
+        // 经过 10^9 天的操作 理论上应该所有整数都存在
+    
+        return n > 1 ? n - 1:1;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 5\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+ */
+
