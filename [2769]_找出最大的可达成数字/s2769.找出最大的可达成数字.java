5fcb6b9646884bb9138c8bb7ac9026404a6bfffc
zj970 <3060529292@qq.com>
2025-01-21 22:45:41 +0800

    .......

---
+++ [2769]_找出最大的可达成数字/s2769.找出最大的可达成数字.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=2769 lang=java
+ * @lcpr version=30204
+ *
+ * [2769] 找出最大的可达成数字
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + t * 2;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// 4\n1\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 3\n2\n
+// @lcpr case=end
+
+ */
+
