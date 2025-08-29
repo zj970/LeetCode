bc32529e05177b7802193304c15022b72b88dd84
zj970 <3060529292@qq.com>
2025-08-29 23:46:15 +0800

    feat(ID3190 runtime 100.00% usage 11.43%) : 📝 枚举

---
+++ leetcode/editor/cn/P3190FindMinimumOperationsToMakeAllElementsDivisibleByThree.java
@@ -0,0 +1,71 @@
+package editor.cn;
+//给你一个整数数组 nums 。一次操作中，你可以将 nums 中的 任意 一个元素增加或者减少 1 。 
+//
+// 请你返回将 nums 中所有元素都可以被 3 整除的 最少 操作次数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+// 输入：nums = [1,2,3,4] 
+// 
+//
+// 输出：3 
+//
+// 解释： 
+//
+// 通过以下 3 个操作，数组中的所有元素都可以被 3 整除： 
+//
+// 
+// 将 1 减少 1 。 
+// 将 2 增加 1 。 
+// 将 4 减少 1 。 
+// 
+//
+// 示例 2： 
+//
+// 
+// 输入：nums = [3,6,9] 
+// 
+//
+// 输出：0 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 50 
+// 1 <= nums[i] <= 50 
+// 
+//
+// Related Topics 数组 数学 👍 6 👎 0
+
+
+/**
+ * @author zj970
+ * @since 2025-08-28 23:37:12
+ */
+public class P3190FindMinimumOperationsToMakeAllElementsDivisibleByThree {
+    public static void main(String[] args) {
+        Solution solution = new P3190FindMinimumOperationsToMakeAllElementsDivisibleByThree().new Solution();
+    }
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int minimumOperations(int[] nums) {
+            int res = 0;
+            for (int value : nums) {
+                if (value % 3 != 0) {
+                    res++;
+                }
+            }
+
+            return res;
+        }
+    }
+    //leetcode submit region end(Prohibit modification and deletion)
+
+}
